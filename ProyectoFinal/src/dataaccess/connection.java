5aa7cd558a87d7d47070400ab05814a8cf6bc635
Leilany12 <60672322+Leilany12@users.noreply.github.com>
2021-12-03 22:24:11 -0600

    Add files via upload

--- ProyectoFinal/src/dataaccess/connection.java
+++ ProyectoFinal/src/dataaccess/connection.java
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 package dataaccess;
 
-import com.mysql.jdbc.Connection;
+import dataaccess.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -24,37 +23,19 @@ class Connection {
         
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            con = (Connection) DriverManager.getConnection(this.DB_URL, this.DB_USERNAME, this.DB_PASSWORD);
-            
-        } catch(SQLException e)
-        {
+            con = (Connection) DriverManager.getConnection(this.DB_URL, this.DB_USERNAME, this.DB_PASSWORD);  
+        } 
+        
+        catch(SQLException e){
             System.err.println(e);
-        } catch (ClassNotFoundException ex) {
+        } 
+        
+        catch (ClassNotFoundException ex) {
             Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
       return con;  
     }
     
 }
-=======
-package dataaccess;
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-
-/**
- *
- * @author maria
- */
-class connection {
-
-    static PreparedStatement prepareStatement(String sql) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    static CallableStatement prepareCall(String sql) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-}
->>>>>>> 7755f904b36f5a91d2f5af0eeb9cc79d0651f40e
